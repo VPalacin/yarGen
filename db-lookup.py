fc3212d85330bf956cc886c14b0f157b01008532
vpalacin <vpalacin@deloitte.com>
2018-10-16 17:29:45 -0300

    Changed syntax to make it Python3 compatible.

--- db-lookup.py
+++ db-lookup.py
@@ -62,70 +62,70 @@ def init_database(lookups):
         # String databases
         if file.startswith("good-strings") and "strings" in lookups:
             try:
-                print "[+] Loading %s ..." % filePath
+                print("[+] Loading %s ..." % filePath)
                 good_pickle = load(get_abs_path(filePath))
                 good_strings_db.update(good_pickle)
-                print "[+] Total: %s / Added %d entries" % (
-                len(good_strings_db), len(good_strings_db) - strings_num)
+                print("[+] Total: %s / Added %d entries" % (
+                len(good_strings_db), len(good_strings_db) - strings_num))
                 strings_num = len(good_strings_db)
-            except Exception, e:
+            except Exception as e:
                 traceback.print_exc()
         # Opcode databases
         if file.startswith("good-opcodes") and "opcodes" in lookups:
             try:
                 if use_opcodes:
-                    print "[+] Loading %s ..." % filePath
+                    print("[+] Loading %s ..." % filePath)
                     good_op_pickle = load(get_abs_path(filePath))
                     good_opcodes_db.update(good_op_pickle)
-                    print "[+] Total: %s (removed duplicates) / Added %d entries" % (
-                    len(good_opcodes_db), len(good_opcodes_db) - opcodes_num)
+                    print("[+] Total: %s (removed duplicates) / Added %d entries" % (
+                    len(good_opcodes_db), len(good_opcodes_db) - opcodes_num))
                     opcodes_num = len(good_opcodes_db)
-            except Exception, e:
+            except Exception as e:
                 use_opcodes = False
                 traceback.print_exc()
         # Imphash databases
         if file.startswith("good-imphash") and "imphash" in lookups:
             try:
-                print "[+] Loading %s ..." % filePath
-                good_imphashes_pickle = load(get_abs_path(filePath))
+                print("[+] Loading %s ..." % filePath
+                good_imphashes_pickle = load(get_abs_path(filePath)))
                 good_imphashes_db.update(good_imphashes_pickle)
-                print "[+] Total: %s / Added %d entries" % (
-                len(good_imphashes_db), len(good_imphashes_db) - imphash_num)
+                print("[+] Total: %s / Added %d entries" % (
+                len(good_imphashes_db), len(good_imphashes_db) - imphash_num))
                 imphash_num = len(good_imphashes_db)
-            except Exception, e:
+            except Exception as e:
                 traceback.print_exc()
         # Export databases
         if file.startswith("good-exports") and "exports" in lookups:
             try:
-                print "[+] Loading %s ..." % filePath
+                print("[+] Loading %s ..." % filePath)
                 good_exports_pickle = load(get_abs_path(filePath))
                 good_exports_db.update(good_exports_pickle)
-                print "[+] Total: %s / Added %d entries" % (
-                len(good_exports_db), len(good_exports_db) - exports_num)
+                print("[+] Total: %s / Added %d entries" % (
+                len(good_exports_db), len(good_exports_db) - exports_num))
                 exports_num = len(good_exports_db)
-            except Exception, e:
+            except Exception as e:
                 traceback.print_exc()
 
 def print_welcome():
-    print Style.RESET_ALL
-    print Fore.WHITE + Back.BLUE
-    print " ".ljust(80)
-    print "                      ______              __                __             ".ljust(80)
-    print "    __  ______ ______/ ____/__  ____     / /   ____  ____  / /____  ______ ".ljust(80)
-    print "   / / / / __ `/ ___/ / __/ _ \\/ __ \\   / /   / __ \\/ __ \\/ //_/ / / / __ \\".ljust(80)
-    print "  / /_/ / /_/ / /  / /_/ /  __/ / / /  / /___/ /_/ / /_/ / ,< / /_/ / /_/ /".ljust(80)
-    print "  \\__, /\\__,_/_/   \\____/\\___/_/ /_/  /_____/\\____/\\____/_/|_|\\__,_/ .___/ ".ljust(80)
-    print " /____/                                                           /_/      ".ljust(80)
-    print " ".ljust(80)
-    print ("  " + __AUTHOR__ + " - " + __VERSION__ + "").ljust(80)
-    print " ".ljust(80) + Style.RESET_ALL
-    print Style.RESET_ALL + " "
+    print(Style.RESET_ALL)
+    print(Fore.WHITE + Back.BLUE)
+    print(" ".ljust(80))
+    print("                      ______              __                __             ".ljust(80))
+    print("    __  ______ ______/ ____/__  ____     / /   ____  ____  / /____  ______ ".ljust(80))
+    print("   / / / / __ `/ ___/ / __/ _ \\/ __ \\   / /   / __ \\/ __ \\/ //_/ / / / __ \\".ljust(80))
+    print("  / /_/ / /_/ / /  / /_/ /  __/ / / /  / /___/ /_/ / /_/ / ,< / /_/ / /_/ /".ljust(80))
+    print("  \\__, /\\__,_/_/   \\____/\\___/_/ /_/  /_____/\\____/\\____/_/|_|\\__,_/ .___/ ".ljust(80))
+    print(" /____/                                                           /_/      ".ljust(80))
+    print(" ".ljust(80))
+    print(("  " + __AUTHOR__ + " - " + __VERSION__ + "").ljust(80))
+    print(" ".ljust(80) + Style.RESET_ALL)
+    print(Style.RESET_ALL + " ")
 
 
 # CTRL+C Handler --------------------------------------------------------------
 def signal_handler(signal_name, frame):
-    print "------------------------------------------------------------------------------\n"
-    print 'INTERRUPTED'
+    print("------------------------------------------------------------------------------\n")
+    print('INTERRUPTED')
     sys.exit(0)
 
 
@@ -149,11 +149,11 @@ if __name__ == '__main__':
 
     # Error checks
     if not args.l:
-        print "[E] You must define at least on lookup module -l [%s]" % " ".join(VALID_LOOKUPS)
+        print("[E] You must define at least on lookup module -l [%s]" % " ".join(VALID_LOOKUPS))
         sys.exit(1)
     for l in args.l[0]:
         if l not in VALID_LOOKUPS:
-            print "[E] '%s' is an unknown lookup - valid lookups are: %s" % (l, ", ".join(VALID_LOOKUPS))
+            print("[E] '%s' is an unknown lookup - valid lookups are: %s" % (l, ", ".join(VALID_LOOKUPS)))
             sys.exit(1)
 
     # Active lookups
@@ -169,10 +169,10 @@ if __name__ == '__main__':
 
     # File input or command line input
     if args.f:
-        print "not yet implemented"
+        print("not yet implemented")
     else:
         # Input loop
-        print "Provide a value (%s) as input" % ", ".join(active_lookpups)
+        print("Provide a value (%s) as input" % ", ".join(active_lookpups))
         while True:
             input_value = raw_input("> ")
 
@@ -205,20 +205,20 @@ if __name__ == '__main__':
             if "imphash" in types:
                 if "imphash" in active_lookpups:
                     if input_value in good_imphashes_db:
-                        print Fore.BLACK, Back.GREEN, "IMPHASH KNOWN GOOD FROM %d FILES" \
-                                                      % good_imphashes_db[input_value], Style.RESET_ALL
+                        print(Fore.BLACK, Back.GREEN, "IMPHASH KNOWN GOOD FROM %d FILES" \
+                                                      % good_imphashes_db[input_value], Style.RESET_ALL)
                     else:
-                        print Fore.BLACK, Back.WHITE, "IMPHASH UNKNOWN", Style.RESET_ALL
+                        print(Fore.BLACK, Back.WHITE, "IMPHASH UNKNOWN", Style.RESET_ALL)
             if "string" in types:
                 if "strings" in active_lookpups:
                     if input_value in good_strings_db:
-                        print Fore.BLACK, Back.GREEN, "STRING KNOWN GOOD IN %d FILES" \
-                                                      % good_strings_db[input_value], Style.RESET_ALL
+                        print(Fore.BLACK, Back.GREEN, "STRING KNOWN GOOD IN %d FILES" \
+                                                      % good_strings_db[input_value], Style.RESET_ALL)
                     else:
-                        print Fore.BLACK, Back.WHITE, "STRING UNKNOWN", Style.RESET_ALL
+                        print(Fore.BLACK, Back.WHITE, "STRING UNKNOWN", Style.RESET_ALL)
                 if "exports" in active_lookpups:
                     if input_value in good_exports_db:
-                        print Fore.BLACK, Back.GREEN, "EXPORT KNOWN GOOD IN %d FILES" \
-                                                      % good_exports_db[input_value], Style.RESET_ALL
+                        print(Fore.BLACK, Back.GREEN, "EXPORT KNOWN GOOD IN %d FILES" \
+                                                      % good_exports_db[input_value], Style.RESET_ALL)
                     else:
-                        print Fore.BLACK, Back.WHITE, "EXPORT UNKNOWN", Style.RESET_ALL
+                        print(Fore.BLACK, Back.WHITE, "EXPORT UNKNOWN", Style.RESET_ALL)
